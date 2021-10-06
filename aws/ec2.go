c7a1f860c88c871171c35430d61b0c8eb0c4d125
Yuzuki <50410851+Yuzuki999@users.noreply.github.com>
2021-10-07 07:17:30 +0800

    删除意外上传的测试用代码

--- aws/ec2.go
+++ aws/ec2.go
@@ -53,8 +53,7 @@ func (p *Aws) CreateEc2(Ami string, Ec2Type string, Name string) (*Ec2Info, erro
 		MaxCount:            aws.Int64(1),
 		KeyName:             &dateName,
 		SecurityGroupIds:    []*string{secRt.GroupId},
-		BlockDeviceMappings: []*ec2.BlockDeviceMapping{{DeviceName: aws.String("/dev/sda1"), Ebs: &ec2.EbsBlockDevice{VolumeSize: aws.Int64(250)}}},
-	}) //创建ec2实例
+		}) //创建ec2实例
 	if runErr != nil {
 		return nil, runErr
 	}
